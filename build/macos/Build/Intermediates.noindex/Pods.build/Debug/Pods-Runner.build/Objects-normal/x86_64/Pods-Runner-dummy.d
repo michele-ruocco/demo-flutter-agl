b462f4755b59edc2feab0243bd6f700175e16121
Raffaele Morlando <raffaele.morlando@kineton.it>
2023-03-21 12:33:13 +0100

    chore: delete build folder and unused dependecies

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/raffaelemorlando/workspace/app/video_player/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/raffaelemorlando/workspace/app/video_player/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/raffaelemorlando/workspace/app/video_player/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
